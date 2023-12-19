218f5328d6df9c1a5994050027c317de75198b4c
Luis <l.delgado@westminster.ac.uk>
2023-12-19 14:25:50 +0000

    AOC using airport terminal to register and request connecting times.

--- agents/airline_operating_centre.py
+++ agents/airline_operating_centre.py
@@ -58,6 +58,7 @@ class AirlineOperatingCentre(Agent):
 		self.aoc_flight_plans = {}  # Flight plans for given flights (flight key)
 		self.aoc_pax_info = {}  # In theory, this should only be used for pax reallocation
 		self.aoc_airports_info = {}  # Information on airports used by flight from the airline
+		self.aoc_airport_terminals_info = {}  # Information on airport terminals used by the passengers from the airline
 		self.aircraft_list = {}  # List of aircraft used by the airline
 		self.other_aoc_flight_plans = {}  # Temporary save flight plan for flights
 		self.aoc_delay_recovery_info = {}  # Information on the delay recovery for a flight aoc gets from pdrp (role in Flight)
@@ -230,8 +231,7 @@ class AirlineOperatingCentre(Agent):
 		Returns a typical turnaround time based on the type of aircraft of flight_uid. Using central registry.
 		"""
 		return self.cr.get_tat(airport_uid, flight_uid)
-		# aircraft = self.aoc_flights_info[flight_uid]['aircraft']
-		# return self.aoc_airports_info[airport_uid]['tats'][aircraft.performances.wtc][self.airline_type]
+
 
 	def get_number_seats_flight(self, flight_uid):
 		"""
@@ -358,24 +358,36 @@ class AirlineOperatingCentre(Agent):
 		"""
 		self.aircraft_list[aircraft.uid] = aircraft
 
-	def register_airport(self, airport, groundhandler_uid):
+	def register_airport(self, airport, airport_terminal_uid=None):
 		"""
 		Add one airport to the list of airports in the AOC
+		"""
+
+		self.aoc_airports_info[airport.uid] = {'airport_terminal_uid': airport_terminal_uid,
+												'avg_taxi_out_time': airport.avg_taxi_out_time,
+												'avg_taxi_in_time': airport.avg_taxi_in_time,
+												'coords': airport.coords,
+												'dman_uid': airport.dman_uid,
+												'curfew': airport.curfew,
+												'ICAO': airport.icao,
+												'tats': airport.tats  # Turnaround distributions
+											   	# 'mcts': airport.mcts, # Now in the dict of airport terminals
+												}
+
+	def register_airport_terminal(self, airport_terminal):
+		"""
+		Add an airport terminal to the entry of airport terminals that the AOC has
 
 		mcts are designed so that one can do:
 		mcts = self.aoc_airports_info[airport_uid]['mcts']
 		mct = mcts[(self.aoc_flights_info[flight_uid1]['international'], self.aoc_flights_info[flight_uid2]['international'])]
 		"""
 
-		self.aoc_airports_info[airport.uid] = { 'groundhandler_uid': groundhandler_uid,
-												'mcts': airport.mcts,
-												'tats': airport.tats,
-												'avg_taxi_out_time': airport.avg_taxi_out_time,
-												'avg_taxi_in_time': airport.avg_taxi_in_time,
-												'coords': airport.coords,
-												'dman_uid': airport.dman_uid,
-												'curfew': airport.curfew,
-												'ICAO': airport.icao}
+		self.aoc_airport_terminals_info[airport_terminal.uid] = {
+											   'mcts': airport_terminal.mcts,
+											   'ICAO': airport_terminal.icao,
+											   }
+
 
 	def register_pax_itinerary_group(self, pax):
 		"""
@@ -1109,7 +1121,7 @@ class AirlineFlightPlanner(Role):
 				for pax in paxs:
 					try:
 						next_flight = pax.get_flight_after(flight_uid)
-						if not next_flight is None:
+						if next_flight is not None:
 							from_time = baseline_ibt + (X-x0) + self.agent.get_mct(flight_uid,
 																					next_flight,
 																					pax.pax_type)
@@ -1835,9 +1847,15 @@ class AirlineFlightPlanner(Role):
 					ft2 = self.agent.aoc_flights_info[flight_uid]['international']
 					pax.previous_flight_international = ft2
 
+					# Get uid of airport where connection is done
 					origin_airport_uid = self.agent.aoc_flights_info[flight_uid]['origin_airport_uid']
-					mcts = self.agent.aoc_airports_info[origin_airport_uid]['mcts'][pax.pax_type]
+					# Get uid of airport terminal for airport
+					airport_terminal_uid = self.agent.aoc_airports_info[origin_airport_uid]['airport_terminal_uid']
+					# Get MCTs from registry of airport terminals
+					mcts = self.agent.aoc_airport_terminals_info[airport_terminal_uid]['mcts'][pax.pax_type]
+					# Check MCTs between specific flights
 					pax.mct = mcts[(ft1, ft2)]
+
 					# self.agent.aph.request_connecting_times(pax, (ft1, ft2))
 
 					# check if ft1 is from the same company as ft2 and calc. its in-block time
@@ -3319,7 +3337,7 @@ class PassengerReallocation(Role):
 			pax.missed_flights.append(flight_uid)
 
 			# Check if pax has taken more time than what the airline was expecting
-			if pax.ct>pax.mct:
+			if pax.ct > pax.mct:
 				# If so, blame the pax for any compensation
 				mprint(pax, 'has taken', pax.ct, 'as connecting time vs. an MCT of', pax.mct,
 						'; it blames itself for any future compensation')
@@ -3806,8 +3824,7 @@ class TurnaroundOperations(Role):
 		msg = Letter()
 		msg['type'] = 'turnaround_request'
 		airport_uid = self.agent.aoc_flights_info[last_flight_uid]['destination_airport_uid']
-		groundhandler_uid = self.agent.aoc_airports_info[airport_uid]['groundhandler_uid']
-		msg['to'] = groundhandler_uid
+		msg['to'] = airport_uid
 		msg['body'] = {'aircraft': aircraft,
 						'ao_type': self.agent.airline_type,
 						'flight_uid': next_flight_uid
@@ -4200,7 +4217,7 @@ class AirlinePaxHandler(Role):
 
 	def request_connecting_times(self, pax, connection_type):
 		msg = Letter()
-		msg['to'] = pax.active_airport
+		msg['to'] = self.agent.aoc_airports_info[pax.active_airport]['airport_terminal_uid']
 		msg['type'] = 'connecting_times_request'
 		msg['body'] = {'pax': pax,
 						'connection_type': connection_type}
@@ -4286,7 +4303,7 @@ class AirlinePaxHandler(Role):
 
 	def wait_for_connecting_times(self, msg):
 		pax = msg['body']['pax']
-		mprint(self.agent, 'receives connecting times for', pax, ':', msg['body']['ct'])
+		mprint(self.agent, 'receives connecting times for', pax, ': (mct:', msg['body']['mct'], ') ', msg['body']['ct'])
 		# pax.mcts.append(msg['body']['mct'])
 		# pax.cts.append(msg['body']['ct'])
 		pax.mct = msg['body']['mct']
