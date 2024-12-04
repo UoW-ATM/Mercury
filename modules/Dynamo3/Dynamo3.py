2be047150321d0478da39599ae05af9982cb3a13
David de la Torre <david.de.la.torre.sangra@upc.edu>
2024-12-04 21:18:19 +0100

    Add Dynamo3 module (dummy).

---
+++ modules/Dynamo3/Dynamo3.py
@@ -0,0 +1,5 @@
+# Define new functions (not methods) that will be dynamically assigned to existing classes or agents.
+# Use self as the first argument of these functions to mimic methods.
+
+def on_init_agent(self):
+    pass
---
+++ modules/Dynamo3/module_definition.py
@@ -0,0 +1,2 @@
+name = 'Dynamo3'
+flavours = []
